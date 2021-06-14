16eb167b4be1214b77e13185043f363a7438be84
kai <kaiwendeveloer78@gmail.com>
2021-06-14 00:45:18 -0400

    [DOTNET] - set up web api service templates and messages broker(RabbitMQ)

---
+++ NET-WebAPI/WebAPI-Publisher/WebAPI-Publisher/WebAPI-Publisher/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Publisher.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI_Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IBusControl _bus;
+
+        public ProductController(IBusControl bus) {
+            _bus = bus;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            Uri uri = new Uri("rabbitmq://localhost/hello-world-queue");
+
+            await Console.Out.WriteLineAsync(product.ProductName);
+            var endPoint = await _bus.GetSendEndpoint(uri);
+            await endPoint.Send(product);
+
+            return Ok(product.ProductName);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NET-WebAPI/WebAPI-Publisher/WebAPI-Publisher/WebAPI-Publisher/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebAPI_Publisher.Models
+{
+    public class User
+    {
+        public string UserEmail { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
