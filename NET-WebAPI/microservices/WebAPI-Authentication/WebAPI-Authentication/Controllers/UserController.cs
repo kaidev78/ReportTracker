db2742cc889bcc94c966de336f1d227190f20e25
kai <kaiwendeveloer78@gmail.com>
2021-07-03 23:47:41 -0400

    [DOTNET] - clean .net codes, add nginx, and add docker compose file for angular and nginx

--- NET-WebAPI/microservices/WebAPI-Authentication/WebAPI-Authentication/Controllers/UserController.cs
+++ NET-WebAPI/microservices/WebAPI-Authentication/WebAPI-Authentication/Controllers/UserController.cs
@@ -32,46 +32,6 @@ namespace WebAPI_Authentication.Controllers
             _tokenRefresher = tokenRefresher;
         }
 
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // GET : test
-        [HttpGet("/test")]
-        public void Test()
-        {
-           
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-
         // POST: api/login
         // login a user
         [HttpPost("/user/login")]
